7a8c706c9d20c820c574dd83bc470da62f1d2e5e
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-12-09 21:29:52 +0100

    Fourth page

---
+++ js/data.js
@@ -0,0 +1,10 @@
+const dataLinks ={
+  
+    header: ["Home", "About", "Services", "Process", "Team", "Blog"],
+    footer: {
+        about: ["The company", "Institutional", "Social & Events", "Innovation", "Environment", "Technology"],
+        services: ["Audit & Assurance", "Financial Advisory", "Analytics M&A", "Middle Marketing", "Legal Consulting", "Regulatory Risk"],
+        support: ["Responsibility","Terms of Use","About Cookies","Privacy Policy","Accessibility","Information"]
+    }
+  
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+let headerData = new Vue({
+  el: ".app",
+  data: {
+      dataLinks,
+      prova: [1,2,3],
+  },
+});
